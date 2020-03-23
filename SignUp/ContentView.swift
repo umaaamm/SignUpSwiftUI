242846b198c009afd0b8244bf73f992b7bfb5f31
Kurniawan Gigih Lutfi Umam <kurniawan.umam@pegadaian.co.id>
2020-03-23 16:09:08 +0700

    Initial Commit

---
+++ SignUp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SignUp
+//
+//  Created by Kurniawan Gigih Lutfi Umam on 23/03/20.
+//  Copyright © 2020 Kurniawan Gigih Lutfi Umam. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
